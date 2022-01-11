537282d24963545ed55b5aebc516db4104c7c04d
mgirish97 <meghanagirish@hotmail.com>
2022-01-10 21:35:15 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+
+# ------------------------------- Blocks ------------------------------- #
+# TODO: Create blocks class
+
+
+# TODO: Make multiple blocks (maybe with different colors)
+
+
+# TODO: Make block disappear when ball hits block
+
+
+# ------------------------------- Paddle ------------------------------- #
+# TODO: Create paddle class
+
+
+# TODO: Create paddle
+
+
+# ------------------------------- Ball ------------------------------- #
+# TODO: Create ball class
+
+
+# TODO: Create ball
+
+
+# TODO: Make ball bounce when it hits the wall, block and paddle.
+#  Determine angle based on length of paddle or block.
+#  Wall would just be the opposite.
+
+
+# ------------------------------- Scoreboard ------------------------------- #
+# TODO: Create Scoreboard class
+
+
+# TODO: Update score. One block hit = one point
+
+
+# TODO: End game when there are no more blocks left.
+
+
+screen.exitonclick()
